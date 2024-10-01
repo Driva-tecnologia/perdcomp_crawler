d4baeb6b7750aaadae27e0f065a3451d00f25f9e
luis henrique <luislopesteo@hotmail.com>
2024-10-01 16:00:14 -0300

    fix : formatting

--- perdcomp.py
+++ perdcomp.py
@@ -2,8 +2,12 @@ from crawler import Crawler, Processor
 from typing import Callable
 
 
-class Perdcomp():
-    def __init__(self, extract_cnpjs_func : Callable[[None], list], save_data_func : Callable[[str], None]):
+class Perdcomp:
+    def __init__(
+        self,
+        extract_cnpjs_func: Callable[[None], list],
+        save_data_func: Callable[[str], None],
+    ):
         self.__result_crawler_path = "data/success"
         self.__result_processed_path = "data/results"
         self.extract_func = extract_cnpjs_func
@@ -15,6 +19,5 @@ class Perdcomp():
         crawler = Crawler(cnpj_list=cnpj_list)
         crawler.run()
         processor = Processor()
-        processor.silver()  
+        processor.silver()
         self.save_data_func(self.__result_processed_path)
-
